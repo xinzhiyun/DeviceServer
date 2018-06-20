f23c922d0744f7959222c59b37ce37095db229f6
潘宏钢 <shipinluzhi@panhonggangdeMacBook-Pro.local>
2018-06-20 10:57:36 +0800

    socket链接

--- Application/Api/Controller/ActionController.class.php
+++ Application/Api/Controller/ActionController.class.php
@@ -8,8 +8,32 @@ class ActionController extends Controller
 {
     public function __construct()
     {
-        parent::__construct();
-        Gateway::$registerAddress = '127.0.0.1:9504';
+        // parent::__construct();
+        // Gateway::$registerAddress = '127.0.0.1:9504';
+        // 设置一些基本的变量
+        $host = "127.0.0.1";
+        $port = 9504;
+        // 设置超时时间
+        set_time_limit(0);
+        // 创建一个Socket
+        $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("创建失败\n");
+        //绑定Socket到端口
+        $result = socket_bind($socket, $host, $port) or die("绑定端口失败\n");
+        // 开始监听链接
+        $result = socket_listen($socket, 3) or die("设置监听失败\n");
+        // accept incoming connections
+        // 另一个Socket来处理通信
+        $spawn = socket_accept($socket) or die("接收失败\n");
+        // 获得客户端的输入
+        $input = socket_read($spawn, 1024) or die("读取失败\n");
+        // 清空输入字符串
+        $input = trim($input);
+        //处理客户端输入并返回结果
+        $output = strrev($input) . "\n";
+        socket_write($spawn, $output, strlen ($output)) or die("输出失败\n");
+        // 关闭sockets
+        socket_close($spawn);
+        socket_close($socket);
     }
 
     public function test()
